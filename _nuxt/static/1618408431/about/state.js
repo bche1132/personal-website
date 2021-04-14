fc2341725ad1b089ce3012265bb5b437d2543b43
Brianna Chen <blchen@mitre.org>
2021-04-14 09:59:53 -0400

    Updates

--- _nuxt/static/1618408431/about/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpersonal-website\u002F_nuxt\u002Fstatic\u002F1618408431",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002Fpersonal-website\u002F",assetsPath:"\u002Fpersonal-website\u002F_nuxt\u002F",cdnURL:a}}}}(null));
